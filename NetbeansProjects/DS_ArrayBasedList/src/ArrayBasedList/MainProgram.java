b59c1488aec0bbd7b03affa16b6aaed56e79e035
Warren D. Rezane <warzkie123@gmail.com>
2018-01-26 22:27:47 +0800

    Deleted extra comments

--- NetbeansProjects/DS_ArrayBasedList/src/ArrayBasedList/MainProgram.java
+++ NetbeansProjects/DS_ArrayBasedList/src/ArrayBasedList/MainProgram.java
@@ -142,46 +142,7 @@ public class MainProgram {
                     
             }
         } while (opt != 11);
-            
-        
-        
-//        /* Insertion */
-//        System.out.println("\n---- Inserting Items... ----");
-//        for(int x=0;x<nl.getListSize();x++){
-//            nl.insert(x);
-//        }
-//        
-//        System.out.println("\nThe list contents are: ");
-//        nl.print();
-        
-//        /* Retrieval */
-//        System.out.println("\n---- Retrieving an array element... ----");
-//        nl.retrieveItem(11);
-//        
-//        /* Searching */
-//        System.out.println("\n---- Searching for an array element... ----");
-//        nl.searchItem(8);
-//        
-//        /* Replacing */
-//        System.out.println("\n---- Replacing an array element... ----");
-//        nl.replaceItem(3, 5);
-//        
-//        System.out.println("\nThe list contents are: ");
-//        nl.print();
-//        
-//        /* Retrieving */
-//        System.out.println("\n---- Retrieving an array element... ----");
-//        nl.retrieveItem(6);
-        
-        /* Removal */
-//        System.out.println("\n---- Deleting 10 Items... ----");
-//        for(int x=0;x<nl.getListSize();x++){
-//            nl.clearList();
-//        }
-//        
-//        System.out.println("\nThe list contents are: ");
-//        nl.print();
-//        nl.clearList();// to check if isEmpty() works
+
     }
     
     static void printMenu() {
