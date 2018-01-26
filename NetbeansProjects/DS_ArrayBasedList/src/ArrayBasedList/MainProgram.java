0d2015ae8f31cb03fb89eca3ad172550fceb19fe
Warren D. Rezane <warzkie123@gmail.com>
2018-01-26 10:39:37 +0800

    Updated Data Structures Excercise

--- NetbeansProjects/DS_ArrayBasedList/src/ArrayBasedList/MainProgram.java
+++ NetbeansProjects/DS_ArrayBasedList/src/ArrayBasedList/MainProgram.java
@@ -1,47 +1,54 @@
 
 package ArrayBasedList;
 
+import java.util.Scanner;
+
 public class MainProgram {
     public static void main(String[] args){
+        Scanner war = new Scanner(System.in);
         NumList nl = new NumList();
-        nl.createList(10);
         
-        /* Insertion */
-        System.out.println("\n---- Inserting 10 Items... ----");
-        for(int x=0;x<nl.getListSize();x++){
-            nl.insert(x);
-        }
+        int limit;
         
-        System.out.println("\nThe list contents are: ");
-        nl.print();
+        System.out.println("[1] ");
         
-        /* Retrieval */
-        System.out.println("\n---- Retrieving an array element... ----");
-        nl.retrieveItem(11);
         
-        /* Searching */
-        System.out.println("\n---- Searching for an array element... ----");
-        nl.searchItem(8);
+//        /* Insertion */
+//        System.out.println("\n---- Inserting Items... ----");
+//        for(int x=0;x<nl.getListSize();x++){
+//            nl.insert(x);
+//        }
+//        
+//        System.out.println("\nThe list contents are: ");
+//        nl.print();
         
-        /* Replacing */
-        System.out.println("\n---- Replacing an array element... ----");
-        nl.replaceItem(3, 5);
-        
-        System.out.println("\nThe list contents are: ");
-        nl.print();
-        
-        /* Retrieving */
-        System.out.println("\n---- Retrieving an array element... ----");
-        nl.retrieveItem(6);
+//        /* Retrieval */
+//        System.out.println("\n---- Retrieving an array element... ----");
+//        nl.retrieveItem(11);
+//        
+//        /* Searching */
+//        System.out.println("\n---- Searching for an array element... ----");
+//        nl.searchItem(8);
+//        
+//        /* Replacing */
+//        System.out.println("\n---- Replacing an array element... ----");
+//        nl.replaceItem(3, 5);
+//        
+//        System.out.println("\nThe list contents are: ");
+//        nl.print();
+//        
+//        /* Retrieving */
+//        System.out.println("\n---- Retrieving an array element... ----");
+//        nl.retrieveItem(6);
         
         /* Removal */
-        System.out.println("\n---- Deleting 10 Items... ----");
-        for(int x=0;x<nl.getListSize();x++){
-            nl.clearList();
-        }
-        
-        System.out.println("\nThe list contents are: ");
-        nl.print();
-        nl.clearList();// to check if isEmpty() works
+//        System.out.println("\n---- Deleting 10 Items... ----");
+//        for(int x=0;x<nl.getListSize();x++){
+//            nl.clearList();
+//        }
+//        
+//        System.out.println("\nThe list contents are: ");
+//        nl.print();
+//        nl.clearList();// to check if isEmpty() works
     }
 }
